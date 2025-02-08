bfe9a55432affba060ce775230459c3a6af9eba1
AI-MoonDongWon <ansehddnjs187@gmail.com>
2025-02-08 20:15:12 +0900

    모델 간소화

--- config/user_settings.py
+++ config/user_settings.py
@@ -7,8 +7,8 @@ setting = {"PRICE_IDX": -1, "window": 50}  # 종가의 위치
 
 network_setting = {
     "d_model": 256,
-    "actor_hidden_dim": 256,
-    "critic_hidden_dim": 256,
+    "actor_hidden_dim": 2048,
+    "critic_hidden_dim": 2048,
     "transformer_layers": 2,
     "nhead": 4,
     "dropout": 0.1,
@@ -20,7 +20,7 @@ trainer_setting = {
     "lr_actor": 3e-4,
     "lr_critic": 1e-3,
     "gamma": 0.99,
-    "K_epochs": 5,
+    "K_epochs": 1,
     "batch_size": 128,
     "eps_clip": 0.2,
     "checkpoint_path": "./weight/policy_weights.pt",
